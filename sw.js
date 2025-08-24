27075baea46d96b91f1cb31f9f903e713d75baa3
StuartHaughey <152048947+StuartHaughey@users.noreply.github.com>
2025-08-24 21:27:43 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'liftlog-cache-v1';
+const CACHE = 'liftlog-cache-v3'; // bump this number each time you change CSS/JS
 self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE)); });
 self.addEventListener('activate', e => { e.waitUntil(self.clients.claim()); });
 self.addEventListener('fetch', e => {
@@ -10,4 +10,5 @@ const fetcher = fetch(req).then(res => { const copy = res.clone(); caches.open(C
 return hit || fetcher;
 })
 );
+
 });
