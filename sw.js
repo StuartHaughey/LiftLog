e992ccb8c1c8a9656fe1880f9881b96aa24cefe8
StuartHaughey <152048947+StuartHaughey@users.noreply.github.com>
2025-08-30 18:32:24 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this when you change JS/CSS/HTML so PWAs fetch fresh files
-const CACHE = 'liftlog-cache-v19';
+const CACHE = 'liftlog-cache-v20';
 
 self.addEventListener('install', (event) => {
   self.skipWaiting();
@@ -37,6 +37,7 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
+
 
 
 
